b1e93cff23bba53277fac501ce11b821f81a6b4e
paireks <47977819+paireks@users.noreply.github.com>
2022-02-04 00:14:56 +0100

    Add components

---
+++ dotbimGH/Components/CreateBimFileGh.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using dotbim;
+using Grasshopper.Kernel;
+
+namespace dotbimGH.Components
+{
+    public class CreateBimFileGh : GH_Component
+    {
+        public CreateBimFileGh()
+            : base("Create BIM File", "Create BIM File", "Creates BIM File", "BIM", "Create File")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Elements Sets", "Elements Sets", "Elements Sets to place in file", GH_ParamAccess.list);
+            pManager.AddGenericParameter("Info", "Info", "Information about file", GH_ParamAccess.item);
+            pManager.AddTextParameter("Path", "Path", "Path to file, should end up with .bim", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<BimElementSet> bimElementSets = new List<BimElementSet>();
+            Dictionary<string, string> info = null;
+            string path = string.Empty;
+            
+            DA.GetDataList(0, bimElementSets);
+            DA.GetData(1, ref info);
+            DA.GetData(2, ref path);
+            
+            File file = Tools.CreateFile(bimElementSets, info);
+            file.Save(path);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("2e9723cf-a0e5-4ec5-b229-e6ab673032a5"); }
+        }
+    }
+}
---
+++ dotbimGH/Components/DeconstructBimFileGh.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using dotbim;
+using Grasshopper.Kernel;
+
+namespace dotbimGH.Components
+{
+    public class DeconstructBimFileGh : GH_Component
+    {
+        public DeconstructBimFileGh()
+            : base("Deconstruct BIM File", "Deconstruct BIM File", "Deconstructs BIM File", "BIM", "Read File")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("BIM File", "BIM File", "BIM File read", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Schema Version", "Schema Version", "Version of schema", GH_ParamAccess.item);
+            pManager.AddGenericParameter("BIM Info", "BIM Info", "BIM Info deconstructed about file", GH_ParamAccess.item);
+            pManager.AddGenericParameter("BIM Elements Geometry", "BIM Elements Geometry", "BIM Elements Geometry deconstructed", GH_ParamAccess.list);
+            pManager.AddGenericParameter("BIM Elements Data", "BIM Elements Data", "BIM Elements Data deconstructed", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            File file = null;
+            DA.GetData(0, ref file);
+
+            DA.SetData(0, file.SchemaVersion);
+            DA.SetData(1, file.Info);
+            DA.SetDataList(2, Tools.ConvertBimMeshesAndElementsIntoRhinoMeshes(file.Meshes, file.Elements));
+            DA.SetDataList(3, file.Elements);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("e7c90a9a-28c2-44b5-a02f-77afae97a5e3"); }
+        }
+    }
+
+}
